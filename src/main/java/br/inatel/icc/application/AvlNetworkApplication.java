2eeab2d08e2afb49720fb6f8c0cccf4017010006
Alexander Augusto <alexaasf_10@hotmail.com>
2021-04-28 07:53:47 -0300

    start spring boot project

---
+++ src/main/java/br/inatel/icc/application/AvlNetworkApplication.java
@@ -0,0 +1,13 @@
+package br.inatel.icc.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvlNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvlNetworkApplication.class, args);
+	}
+
+}
