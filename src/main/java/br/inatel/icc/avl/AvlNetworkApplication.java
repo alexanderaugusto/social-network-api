d1f6078686503e47ec64afef5d91a1527ae1556a
Alexander Augusto <alexaasf_10@hotmail.com>
2021-04-28 08:34:58 -0300

    modify main package

--- src/main/java/br/inatel/icc/avl/AvlNetworkApplication.java
+++ src/main/java/br/inatel/icc/avl/AvlNetworkApplication.java
@@ -1,4 +1,4 @@
-package br.inatel.icc.application;
+package br.inatel.icc.avl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
