339ba410529a8edd73b37bb4cf175db1f7868966
Alexander Augusto <alexaasf_10@hotmail.com>
2021-04-28 14:12:06 -0300

    add email validation to email field

--- src/main/java/br/inatel/icc/avl/controller/form/UserForm.java
+++ src/main/java/br/inatel/icc/avl/controller/form/UserForm.java
@@ -1,11 +1,22 @@
 package br.inatel.icc.avl.controller.form;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.sun.istack.NotNull;
+
 import br.inatel.icc.avl.model.User;
 
 public class UserForm {
+	@NotNull @NotEmpty @Length(min = 5)
 	private String name;
+	@NotNull @NotEmpty @Email
 	private String email;
+	@NotNull @NotEmpty @Length(min = 8)
 	private String password;
+	@NotEmpty @Length(min = 9)
 	private String phone;
 	
 	public String getName() {
