bbac0c939ffdf14d7b40b5c7db743d0443f8ca84
Alexander Augusto <alexaasf_10@hotmail.com>
2021-04-29 14:10:25 -0300

    add annotation to enable data web support to use pagination parameters in request

--- src/main/java/br/inatel/icc/avl/AvlNetworkApplication.java
+++ src/main/java/br/inatel/icc/avl/AvlNetworkApplication.java
@@ -2,8 +2,10 @@ package br.inatel.icc.avl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class AvlNetworkApplication {
 
 	public static void main(String[] args) {
