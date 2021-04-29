1adfa78089c46dd5b79274d90a3c2d2f11fecde7
Alexander Augusto <alexaasf_10@hotmail.com>
2021-04-29 15:34:24 -0300

    add PostDetailDto to filter Post model with owner, comments and reactions

---
+++ src/main/java/br/inatel/icc/avl/controller/dto/PostDetailDto.java
@@ -0,0 +1,55 @@
+package br.inatel.icc.avl.controller.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.inatel.icc.avl.model.Post;
+
+public class PostDetailDto {
+
+	private Long id;
+	private String description;
+	private String media;
+	private UserDto owner;
+	private List<CommentDto> comments;
+	private List<ReactionDto> reactions;
+	
+	public PostDetailDto(Post post) {
+		this.id = post.getId();
+		this.description = post.getDescription();
+		this.media = post.getMedia();
+		this.owner = new UserDto(post.getOwner());
+		
+		this.comments = new ArrayList<>();
+		this.comments.addAll(post.getComments().stream().map(CommentDto::new).collect(Collectors.toList()));
+		
+		this.reactions = new ArrayList<>();
+		this.reactions.addAll(post.getReactions().stream().map(ReactionDto::new).collect(Collectors.toList()));
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getMedia() {
+		return media;
+	}
+
+	public UserDto getOwner() {
+		return owner;
+	}
+
+	public List<CommentDto> getComments() {
+		return comments;
+	}
+
+	public List<ReactionDto> getReactions() {
+		return reactions;
+	}
+}
