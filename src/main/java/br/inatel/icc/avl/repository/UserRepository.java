2e6922b2f27516a55ce3e4bb82ac44f8b9f53a2f
Alexander Augusto <alexaasf_10@hotmail.com>
2021-04-29 14:12:08 -0300

    add method to UserRepository to find users that containing a name

--- src/main/java/br/inatel/icc/avl/repository/UserRepository.java
+++ src/main/java/br/inatel/icc/avl/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package br.inatel.icc.avl.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.inatel.icc.avl.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-
+	Page<User> findByNameContainingIgnoreCase(String userName, Pageable pageable);
 }
