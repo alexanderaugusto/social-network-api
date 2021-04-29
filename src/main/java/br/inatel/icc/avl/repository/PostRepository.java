ee6ee630a8aeff517a5b6a06f5ada41ddcfa51bb
Alexander Augusto <alexaasf_10@hotmail.com>
2021-04-29 15:35:28 -0300

    create PostRepository to use jpa in Post model

---
+++ src/main/java/br/inatel/icc/avl/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package br.inatel.icc.avl.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.inatel.icc.avl.model.Post;
+
+public interface PostRepository extends JpaRepository<Post, Long>{
+
+}
