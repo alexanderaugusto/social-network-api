b1045575694aebc4a7229a34b1ce2932c5c8eb84
Alexander Augusto <alexaasf_10@hotmail.com>
2021-04-29 08:25:47 -0300

    change followers and followings insertion

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -23,7 +23,7 @@ VALUES
 ('kkkkkkkkkk ri demais', 3, 2);
 
 INSERT INTO USER_FOLLOWERS (user_id, follower_id)
-VALUES (1, 2), (1, 3), (1, 4), (3, 4);
+VALUES (2, 1), (3, 1), (3, 2), (4, 3);
 
 INSERT INTO USER_FOLLOWINGS (user_id, following_id)
 VALUES (1, 2), (1, 3), (2, 3), (3, 4);
