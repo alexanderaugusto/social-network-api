21fa5d1b39605e46f060487af49c1d1454f03601
Alexander Augusto <alexaasf_10@hotmail.com>
2021-04-30 08:56:31 -0300

    add id property to CommentDto

--- src/main/java/br/inatel/icc/avl/controller/dto/CommentDto.java
+++ src/main/java/br/inatel/icc/avl/controller/dto/CommentDto.java
@@ -4,12 +4,18 @@ import br.inatel.icc.avl.model.Comment;
 
 public class CommentDto {
 
+	private Long id;
 	private String description;
 	private UserDto user;
 	
 	public CommentDto(Comment comment) {
+		this.id = comment.getId();
 		this.description = comment.getDescription();
 		this.user = new UserDto(comment.getUser());
+	}
+
+	public Long getId() {
+		return id;
 	}
 
 	public String getDescription() {
