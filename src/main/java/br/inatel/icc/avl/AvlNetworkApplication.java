ff93916c1d0cbb79dee5716188d1b4833fdaaa36
Alexander Augusto <alexaasf_10@hotmail.com>
2021-04-30 16:50:12 -0300

    enable cache in the application

--- src/main/java/br/inatel/icc/avl/AvlNetworkApplication.java
+++ src/main/java/br/inatel/icc/avl/AvlNetworkApplication.java
@@ -2,10 +2,12 @@ package br.inatel.icc.avl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@EnableCaching
 public class AvlNetworkApplication {
 
 	public static void main(String[] args) {
