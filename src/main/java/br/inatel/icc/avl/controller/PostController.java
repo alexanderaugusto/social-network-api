3632561cb97847aa9c82eb84d7480d8960208bf6
Alexander Augusto <alexaasf_10@hotmail.com>
2021-04-30 08:56:12 -0300

    user can react and remove reaction from post, and add comment and remove comment from post

--- src/main/java/br/inatel/icc/avl/controller/PostController.java
+++ src/main/java/br/inatel/icc/avl/controller/PostController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +20,15 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.inatel.icc.avl.controller.dto.PostDetailDto;
 import br.inatel.icc.avl.controller.dto.PostDto;
+import br.inatel.icc.avl.controller.form.CommentForm;
 import br.inatel.icc.avl.controller.form.PostForm;
+import br.inatel.icc.avl.model.Comment;
 import br.inatel.icc.avl.model.Post;
+import br.inatel.icc.avl.model.Reaction;
 import br.inatel.icc.avl.model.User;
+import br.inatel.icc.avl.repository.CommentRepository;
 import br.inatel.icc.avl.repository.PostRepository;
+import br.inatel.icc.avl.repository.ReactionRepository;
 import br.inatel.icc.avl.repository.UserRepository;
 
 @RestController
@@ -31,49 +37,113 @@ public class PostController {
 
 	private PostRepository postRepository;
 	private UserRepository userRepository;
+	private ReactionRepository reactionRepository;
+	private CommentRepository commentRepository;
 	private final long myId = 1;
-	
+
 	@Autowired
-	public PostController(PostRepository postRepository, UserRepository userRepository) {
+	public PostController(PostRepository postRepository, UserRepository userRepository,
+			ReactionRepository reactionRepository, CommentRepository commentRepository) {
 		this.postRepository = postRepository;
 		this.userRepository = userRepository;
+		this.reactionRepository = reactionRepository;
+		this.commentRepository = commentRepository;
 	}
-	
+
 	@PostMapping
 	@Transactional
-	public ResponseEntity<PostDto> create(@RequestBody @Valid PostForm postForm, UriComponentsBuilder uriBuilder){
+	public ResponseEntity<PostDto> create(@RequestBody @Valid PostForm postForm, UriComponentsBuilder uriBuilder) {
 		User user = userRepository.getOne(myId);
-		
+
 		Post post = postForm.toPost(user);
 		postRepository.save(post);
-		
+
 		URI uri = uriBuilder.path("/posts/{id}").buildAndExpand(post.getId()).toUri();
-		
+
 		return ResponseEntity.status(201).location(uri).body(new PostDto(post));
 	}
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<PostDetailDto> list(@PathVariable("id") Long id){
+	public ResponseEntity<PostDetailDto> list(@PathVariable("id") Long id) {
+		Optional<Post> post = postRepository.findById(id);
+
+		if (post.isPresent()) {
+			PostDetailDto postDto = new PostDetailDto(post.get());
+			return ResponseEntity.status(200).body(postDto);
+		}
+
+		return ResponseEntity.status(404).build();
+	}
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+		Optional<Post> post = postRepository.findById(id);
+
+		if (post.isPresent()) {
+			postRepository.deleteById(id);
+			return ResponseEntity.status(204).build();
+		}
+
+		return ResponseEntity.status(404).build();
+	}
+
+	@PutMapping("/{id}/reactions")
+	@Transactional
+	public ResponseEntity<?> addReaction(@PathVariable("id") Long id) {
+		User user = userRepository.getOne(myId);
+		Optional<Post> post = postRepository.findById(id);
+
+		if (post.isPresent()) {
+			Reaction reaction = new Reaction(user, post.get());
+			reactionRepository.save(reaction);
+			return ResponseEntity.status(204).build();
+		}
+
+		return ResponseEntity.status(404).build();
+	}
+
+	@DeleteMapping("/{id}/reactions")
+	@Transactional
+	public ResponseEntity<?> removeReaction(@PathVariable("id") Long id) {
+		User user = userRepository.getOne(myId);
+		Optional<Post> post = postRepository.findById(id);
+
+		if (post.isPresent()) {
+			Reaction reaction = reactionRepository.findByUserAndPost(user, post.get());
+			reactionRepository.deleteById(reaction.getId());
+			return ResponseEntity.status(204).build();
+		}
+
+		return ResponseEntity.status(404).build();
+	}
+
+	@PutMapping("/{id}/comments")
+	@Transactional
+	public ResponseEntity<?> addComment(@RequestBody @Valid CommentForm form, @PathVariable("id") Long id){
+		User user = userRepository.getOne(myId);
 		Optional<Post> post = postRepository.findById(id);
 		
 		if(post.isPresent()) {
-			PostDetailDto postDto = new PostDetailDto(post.get());
-			return ResponseEntity.status(200).body(postDto);
+			Comment comment = form.toComment(user, post.get());
+			commentRepository.save(comment);
+			return ResponseEntity.status(204).build();
 		}
 		
 		return ResponseEntity.status(404).build();
 	}
-	
-	@DeleteMapping("/{id}")
+
+	@DeleteMapping("/{id}/comments/{commentId}")
 	@Transactional
-	public ResponseEntity<?> delete(@PathVariable("id") Long id){
+	public ResponseEntity<?> removeComment(@PathVariable("id") Long id, @PathVariable("commentId") Long commentId) {
 		Optional<Post> post = postRepository.findById(id);
+		Optional<Comment> comment = commentRepository.findById(commentId);
 		
-		if(post.isPresent()) {
-			postRepository.deleteById(id);
+		if (post.isPresent() && comment.isPresent()) {
+			commentRepository.deleteById(commentId);
 			return ResponseEntity.status(204).build();
 		}
-		
+
 		return ResponseEntity.status(404).build();
 	}
 }
