0bd669d184f811bf3d82520bb798c64866fe095b
Alexander Augusto <alexaasf_10@hotmail.com>
2021-04-30 17:13:57 -0300

    add method to convert Post list to PostDetailDto list

--- src/main/java/br/inatel/icc/avl/controller/dto/PostDetailDto.java
+++ src/main/java/br/inatel/icc/avl/controller/dto/PostDetailDto.java
@@ -1,6 +1,5 @@
 package br.inatel.icc.avl.controller.dto;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,26 +10,17 @@ public class PostDetailDto {
 	private Long id;
 	private String description;
 	private String media;
-	private UserDto owner;
 	private int totalReactions;
 	private int totalComments;
-	private List<CommentDto> comments;
-	private List<ReactionDto> reactions;
+	private UserDto owner;
 	
 	public PostDetailDto(Post post) {
 		this.id = post.getId();
 		this.description = post.getDescription();
 		this.media = post.getMedia();
-		this.owner = new UserDto(post.getOwner());
 		this.totalReactions = post.getReactions().size();
 		this.totalComments = post.getComments().size();
-		
-		this.comments = new ArrayList<>();
-		this.comments.addAll(post.getComments().stream().map(CommentDto::new).collect(Collectors.toList()));
-		
-		this.reactions = new ArrayList<>();
-		this.reactions.addAll(post.getReactions().stream().map(ReactionDto::new).collect(Collectors.toList()));
-		
+		this.owner = new UserDto(post.getOwner());
 	}
 
 	public Long getId() {
@@ -57,11 +47,8 @@ public class PostDetailDto {
 		return totalComments;
 	}
 
-	public List<CommentDto> getComments() {
-		return comments;
-	}
-
-	public List<ReactionDto> getReactions() {
-		return reactions;
+	public static List<PostDetailDto> toDtoList(List<Post> posts) {
+		List<PostDetailDto> postsDto = posts.stream().map(PostDetailDto::new).collect(Collectors.toList());
+		return postsDto;
 	}
 }
