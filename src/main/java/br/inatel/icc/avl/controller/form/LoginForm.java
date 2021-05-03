57cbfffdaac458606d8c61639624db4358efb7f0
Alexander Augusto <alexaasf_10@hotmail.com>
2021-05-03 11:49:06 -0300

    create LoginForm to receive email and password from client

---
+++ src/main/java/br/inatel/icc/avl/controller/form/LoginForm.java
@@ -0,0 +1,21 @@
+package br.inatel.icc.avl.controller.form;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+public class LoginForm {
+
+	private String email;
+	private String password;
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public UsernamePasswordAuthenticationToken toLoginData() {
+		return new UsernamePasswordAuthenticationToken(email, password);
+	}
+}
