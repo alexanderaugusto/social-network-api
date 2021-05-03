a9205ded61de2079dbf5c7b424456e38b1f82498
Alexander Augusto <alexaasf_10@hotmail.com>
2021-05-03 09:01:48 -0300

    implements UserDetail inferface and override methods to authenticate user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,13 +7,13 @@ VALUES
 
 INSERT INTO POST (description, media, owner_id)
 VALUES 
-('Olha essa música, é muito massa!!!', 'https://www.youtube.com/watch?v=kPBzTxZQG5Q', 1),
+('Olha essa música, é muito massa!!!', 'https://i.pinimg.com/originals/da/ac/6f/daac6f2d4d76503c7d9810e808e59e63.jpg', 1),
 ('Não encontrei nada :(', 'https://i.pinimg.com/originals/da/ac/6f/daac6f2d4d76503c7d9810e808e59e63.jpg', 1),
 ('Procuro pessoas para me ensinar a programar, pago bem :D', null, 3),
 ('Se gostou, deixa o like!', 'https://i.pinimg.com/736x/39/5c/cb/395ccb62c91e09fc7507c0e5597d7fe1.jpg', 2);
 
 INSERT INTO REACTION (user_id, post_id)
-VALUES (2, 1), (3, 1), (4, 1), (1, 2), (3, 2), (4, 3);
+VALUES (2, 1), (3, 1), (4, 1), (1, 2), (3, 2), (4, 3), (1, 4);
 
 INSERT INTO COMMENT (description, user_id, post_id)
 VALUES 
