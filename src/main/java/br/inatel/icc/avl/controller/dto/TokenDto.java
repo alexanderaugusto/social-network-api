c203048d6a11a7ae21942c046ee68d592b255aa8
Alexander Augusto <alexaasf_10@hotmail.com>
2021-05-03 11:48:48 -0300

    create TokenDto to return token and authentication type to client

---
+++ src/main/java/br/inatel/icc/avl/controller/dto/TokenDto.java
@@ -0,0 +1,20 @@
+package br.inatel.icc.avl.controller.dto;
+
+public class TokenDto {
+	
+	private String token;
+	private String type;
+	
+	public TokenDto(String token, String type) {
+		this.token = token;
+		this.type = type;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public String getType() {
+		return type;
+	}
+}
