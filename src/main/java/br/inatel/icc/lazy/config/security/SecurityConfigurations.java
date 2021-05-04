1bd5ac7b44f335ac76aab170a9981242f090ab09
Alexander Augusto <alexaasf_10@hotmail.com>
2021-05-04 16:30:49 -0300

    add prod profile to SecurityConfigurations

--- src/main/java/br/inatel/icc/lazy/config/security/SecurityConfigurations.java
+++ src/main/java/br/inatel/icc/lazy/config/security/SecurityConfigurations.java
@@ -3,6 +3,7 @@ package br.inatel.icc.lazy.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -18,6 +19,7 @@ import br.inatel.icc.lazy.repository.UserRepository;
 
 @EnableWebSecurity
 @Configuration
+@Profile("prod")
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
 	private AuthService authService;
