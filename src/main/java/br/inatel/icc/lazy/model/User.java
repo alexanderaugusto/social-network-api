62765895812e94790f6832497699c6a5c5193c42
Alexander Augusto <alexaasf_10@hotmail.com>
2021-05-05 14:40:39 -0300

    change entity name

--- src/main/java/br/inatel/icc/lazy/model/User.java
+++ src/main/java/br/inatel/icc/lazy/model/User.java
@@ -12,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
@@ -19,6 +20,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
+@Table(name="\"user\"")
 public class User implements UserDetails{
 
 	private static final long serialVersionUID = 1L;
