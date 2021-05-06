257f18382337ff4b38f6c20775eb03ba9f7b6ee3
Alexander Augusto <alexaasf_10@hotmail.com>
2021-05-06 11:52:38 -0300

    changes in PostForm

--- src/main/java/br/inatel/icc/lazy/controller/form/PostForm.java
+++ src/main/java/br/inatel/icc/lazy/controller/form/PostForm.java
@@ -1,13 +1,17 @@
 package br.inatel.icc.lazy.controller.form;
 
+import java.io.Serializable;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import br.inatel.icc.lazy.model.Post;
 import br.inatel.icc.lazy.model.User;
 
-public class PostForm {
+public class PostForm implements Serializable{
 
+	private static final long serialVersionUID = 1L;
+	
 	@NotNull @NotEmpty
 	private String description;
 	private String media;
