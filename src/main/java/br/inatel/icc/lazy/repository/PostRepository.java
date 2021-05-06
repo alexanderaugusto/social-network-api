9993a297c9d16d5ca1d2df868638b7dfcbbdcb52
Alexander Augusto <alexaasf_10@hotmail.com>
2021-05-06 16:54:51 -0300

    change finded table User to Users

--- src/main/java/br/inatel/icc/lazy/repository/PostRepository.java
+++ src/main/java/br/inatel/icc/lazy/repository/PostRepository.java
@@ -11,7 +11,7 @@ import br.inatel.icc.lazy.model.Post;
 public interface PostRepository extends JpaRepository<Post, Long>{
 
 	@Query(value = "SELECT * FROM Post \r\n"
-			+ "INNER JOIN User ON Post.owner_id = User.id \r\n"
+			+ "INNER JOIN Users ON Post.owner_id = Users.id \r\n"
 			+ "INNER JOIN Follow ON Post.owner_id = Follow.follower_id \r\n"
 			+ "WHERE Follow.following_id = :id ORDER BY Post.id DESC",
 			nativeQuery = true)
