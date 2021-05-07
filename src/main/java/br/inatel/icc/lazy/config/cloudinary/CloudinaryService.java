2b1e030072754126221b3173d50f734644f955a9
Alexander Augusto <alexaasf_10@hotmail.com>
2021-05-07 11:25:01 -0300

    pass cloudinary info to enviroment variable

--- src/main/java/br/inatel/icc/lazy/config/cloudinary/CloudinaryService.java
+++ src/main/java/br/inatel/icc/lazy/config/cloudinary/CloudinaryService.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,17 +18,20 @@ import com.cloudinary.utils.ObjectUtils;
 public class CloudinaryService {
 
 	private Cloudinary cloudinary;
-	
+
 	@Value("${lazy.cloudinary.default}")
 	private String cloudinaryDefault;
-	
-	public CloudinaryService() {
+
+	@Autowired
+	public CloudinaryService(@Value("${lazy.cloudinary.cloud_name}") String cloudName,
+			@Value("${lazy.cloudinary.api_key}") String apiKey,
+			@Value("${lazy.cloudinary.api_secret}") String apiSecret) {
 		Map<String, String> valuesMap = new HashMap<>();
 		
-		valuesMap.put("cloud_name", "alexanderaugusto");
-		valuesMap.put("api_key", "424573627157699");
-		valuesMap.put("api_secret", "3qyzJoneW9dmUI-yQODv1AN7sZg");
-		
+		valuesMap.put("cloud_name", cloudName);
+		valuesMap.put("api_key", apiKey);
+		valuesMap.put("api_secret", apiSecret);
+
 		cloudinary = new Cloudinary(valuesMap);
 	}
 
@@ -35,21 +39,11 @@ public class CloudinaryService {
 		return cloudinaryDefault;
 	}
 
-	public void setCloudinaryDefault(String cloudinaryDefault) {
-		this.cloudinaryDefault = cloudinaryDefault;
-	}
-
-
-
 	@SuppressWarnings("rawtypes")
 	public Map upload(MultipartFile multipartFile, String folder) throws IOException {
 		File file = convert(multipartFile);
-		Map params = ObjectUtils.asMap(
-		    "folder", cloudinaryDefault + "/" + folder,
-		    "unique_filename", true,
-		    "overwrite", true,
-		    "resource_type", "image"         
-		);
+		Map params = ObjectUtils.asMap("folder", cloudinaryDefault + "/" + folder, "unique_filename", true, "overwrite",
+				true, "resource_type", "image");
 		Map result = cloudinary.uploader().upload(file, params);
 		file.delete();
 		return result;
