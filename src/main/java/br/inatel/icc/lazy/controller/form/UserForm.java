1d6bc2f6cbdda0bc8eae99b5fc0c2abe035b8824
Alexander Augusto <alexaasf_10@hotmail.com>
2021-05-10 21:08:03 -0300

    add set mothods

--- src/main/java/br/inatel/icc/lazy/controller/form/UserForm.java
+++ src/main/java/br/inatel/icc/lazy/controller/form/UserForm.java
@@ -19,21 +19,37 @@ public class UserForm {
 	@NotNull @NotEmpty @Length(min = 8)
 	private String password;
 	private MultipartFile avatar;
-	
+
 	public String getName() {
 		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getEmail() {
 		return email;
 	}
-	
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	public String getPassword() {
 		return password;
 	}
 
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public MultipartFile getAvatar() {
 		return avatar;
+	}
+
+	public void setAvatar(MultipartFile avatar) {
+		this.avatar = avatar;
 	}
 
 	public User toUser(String avatarUrl) {
